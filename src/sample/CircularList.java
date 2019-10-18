c3b3be82529bce2df87f3de2aeca707b245808c1
anna <yanshina.ann@gmail.com>
2019-10-18 19:27:02 +0300

    new

--- src/sample/CircularList.java
+++ src/sample/CircularList.java
@@ -1,73 +1,22 @@
 package sample;
+import java.util.ArrayList;
 
-public class CircularList<T> {
-    public int size;
-    //Represents the node of list.
-    public class Node{
-        T data;
+public class CircularList<E> extends ArrayList<E>
+{
+    private static final long serialVersionUID = 1L;
 
-        Node next;
-        public Node(T data) {
-            this.data = data;
+    public E get(int index)
+    {
+        if (index == -1)
+        {
+            index = size()-1;
         }
+
+        else if (index == size())
+        {
+            index = 0;
+        }
+
+        return super.get(index);
     }
-
-    //Declaring head and tail pointer as null.
-    public Node head = null;
-    public Node tail = null;
-
-    //This function will add the new node at the end of the list.
-    public void add(T data){
-        //Create new node
-        Node newNode = new Node(data);
-        //Checks if the list is empty.
-        if(head == null) {
-            //If list is empty, both head and tail would point to new node.
-            head = newNode;
-            tail = newNode;
-            newNode.next = head;
-        }
-        else {
-            //tail will point to new node.
-            tail.next = newNode;
-            //New node will become new tail.
-            tail = newNode;
-            //Since, it is circular linked list tail will point to head.
-            tail.next = head;
-        }
-        size ++;
-    }
-
-    public T elementAt(int index){
-        if(index>size){
-            return null;
-        }
-        Node n = head;
-        while(index-1!=0){
-            n=n.next;
-            index--;
-        }
-        return n.data;
-    }
-
-    public int getSize(){
-        return size;
-    }
-    //Displays all the nodes in the list
-    public void display() {
-        Node current = head;
-        if(head == null) {
-            System.out.println("List is empty");
-        }
-        else {
-            System.out.println("Nodes of the circular linked list: ");
-            do{
-                //Prints each node by incrementing pointer.
-                System.out.print(" "+ current.data.toString());
-                current = current.next;
-            }while(current != head);
-            System.out.println();
-        }
-    }
-
 }
